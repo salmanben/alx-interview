be20b1689b0ea356ab60ec1ff2dac2b9e4529412
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2024-05-22 14:50:00 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 
 import sys
 
-
 def print_msg(dict_sc, total_file_size):
     """
     Method to print
@@ -12,12 +11,10 @@ def print_msg(dict_sc, total_file_size):
     Returns:
         Nothing
     """
-
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
             print("{}: {}".format(key, val))
-
 
 total_file_size = 0
 code = 0
@@ -33,20 +30,20 @@ dict_sc = {"200": 0,
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  
-        parsed_line = parsed_line[::-1]  
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
         if len(parsed_line) > 2:
             counter += 1
 
             if counter <= 10:
-                total_file_size += int(parsed_line[0])  
-                code = parsed_line[1] 
+                total_file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-                if (code in dict_sc.keys()):
+                if code in dict_sc.keys():
                     dict_sc[code] += 1
 
-            if (counter == 10):
+            if counter == 10:
                 print_msg(dict_sc, total_file_size)
                 counter = 0
 
